4455ec751cf2353143382ea0ee111b340dc4879a
Lisboa <lisboaneto56@gmail.com>
2024-12-29 21:17:49 -0300

    Terminei a primeira página

--- script.js
+++ script.js
@@ -114,3 +114,21 @@ document.getElementById('right').addEventListener('click', next);
 document.getElementById('left').addEventListener('click', previous);
 
 atualizarImagemFeedback()
+
+/* botao */
+
+const btnTopo = document.getElementById('top');
+
+    // Exibe o botão quando o usuário rolar 300px ou mais
+    window.onscroll = function() {
+      if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        btnTopo.classList.add('show');
+      } else {
+        btnTopo.classList.remove('show');
+      }
+    };
+
+    // Quando o botão é clicado, rola até o topo
+    btnTopo.onclick = function() {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
