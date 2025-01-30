06a78e1f8a02950aef27688b6395e43ae5572602
Lisboa <lisboaneto56@gmail.com>
2025-01-30 00:28:26 -0300

    erro da imagem arrumado

---
+++ darkmode.js
@@ -0,0 +1,12 @@
+/* quando o botao for de dark mode for clicado */
+
+let darkmode = document.getElementById("dark")
+let fundo = document.body
+let article1_p1 = document.getElementById("sobre")
+let article1_p1_t1 = document.getElementById("texto_sobre")
+
+function mode(){
+    
+    article1_p1.style.backgroundColor = "#584032"
+    
+}
